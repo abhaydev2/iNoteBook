a7d82dac78742ac2b24fefff107179b222a3d7b3
abhaydev2 <166593419+abhaydev2@users.noreply.github.com>
2025-07-27 23:17:01 +0545

    send announcement not working

--- iNoteBook/backend/routes/adminRoutes.js
+++ iNoteBook/backend/routes/adminRoutes.js
@@ -15,7 +15,7 @@ router.post('/logout', logoutAdmin);
 // GET /api/admin/dashboard-data
 router.get('/dashboard-data', getData);
 
-router.post('/send-announcement', sendAnouncement);
+router.post('/send-nnouncement', sendAnouncement);
 
 router.post('/add-admin', addAdmin);
 
